d40743116aa7552102d4387959280a0a0cffaecc
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-07 22:00:23 +0530

    Index print equal to target.

---
+++ indexprint.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,target;
+    scanf("%d %d",&n,&target);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i]+arr[j]==target){
+                printf("[%d, %d]",i,j);
+                return 0;
+            }
+        }
+    }
+}
