fe4fa78dc856520c0e59f381ad2cb63333ee1d36
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-07 14:15:01 +0530

    Remove element that are greater than the target.

---
+++ remelementinnewarray.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,target;
+    scanf("%d %d",&n,&target);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]>=target){
+            continue;
+        }
+        printf("%d ",arr[i]);
+    }
+}
