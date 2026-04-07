3df8a6abe4f587b4da12a04ae4095b30a64fcd29
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-07 12:01:41 +0530

    left rotation

---
+++ 1.leftrotate.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,rot;
+    scanf("%d %d",&n,&rot);
+    int arr[n],res[n];
+    int k;
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+        for(int i=0;i<n;i++){
+            arr[i]=res[(i+rot)%n];
+        }
+        for(int i=0;i<n;i++){
+            printf("%d ",res[i]);
+        }
+
+}
