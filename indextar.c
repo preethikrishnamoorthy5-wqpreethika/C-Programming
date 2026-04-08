a747b596c8e81566de4b7e484584059d1cf9b75e
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-08 10:18:04 +0530

    Print target index.

---
+++ indextar.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n,target;
+    scanf("%d %d",&n,&target);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]==target){
+            printf("%d",i);
+            break;
+        }
+    }
+        printf("\n");
+        for(int i=0;i<n;i++){
+            if(arr[i]!=target){
+                printf("%d ",arr[i]);
+            }
+        }
+}
