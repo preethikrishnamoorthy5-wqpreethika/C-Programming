1502a8b9e0f60cf2720b21bf33e8cd2b3b07955a
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-08 09:30:52 +0530

    Print odd and even.

---
+++ oddandeven.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,temp;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+             if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+             }
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]%2==0){
+            printf("%d ",arr[i]);
+        }
+    }
+    printf("\n");
+    for(int i=0;i<n;i++){
+        if(arr[i]%2!=0){
+            printf("%d ",arr[i]);
+        }
+    }
+}
