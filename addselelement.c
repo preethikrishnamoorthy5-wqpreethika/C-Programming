f61248da4fbef272da8f9924b797c29970496c23
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-08 10:45:25 +0530

    Add the selected element.

---
+++ addselelement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,sum=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        sum=arr[0]+arr[1]+arr[n-1]+arr[n-2];
+    }
+    printf("%d",sum);
+}
