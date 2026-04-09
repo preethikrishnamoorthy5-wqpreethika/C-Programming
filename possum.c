bf3b713124a95389d9406df0f376fe6ed05e0b13
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-09 18:34:46 +0530

    Print sum of Positive numbers.

---
+++ possum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }int sum=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>0){
+            sum+=arr[i];
+            continue;
+        }
+    }
+    printf("%d",sum);
+    printf("\n");
+    for(int i=0;i<n;i++){
+        if(arr[i]>0){
+    printf("%d ",arr[i]);}
+        }
+}
