3df9cc93993d711c44aeb8c73c60de29bf65587a
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-09 09:40:21 +0530

    Median of an array.

---
+++ median.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int median,n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<=n;j++){
+            if(arr[i]>arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    
+        if(n%2==0){
+             median=(arr[(n/2)-1]+arr[n/2])/2;
+        }
+        else{
+            median=arr[n/2];
+        }
+    
+    printf("%d",median);
+}
