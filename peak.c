a223d6082443aa4d3813514710294e3b2ec46a4e
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-10 14:19:02 +0530

    Print peak numbers.

---
+++ peak.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=1;i<n-1;i++){
+        if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+            printf("%d ",arr[i]);
+        }
+    }
+    if(arr[0]>arr[1]){
+       printf("%d ",arr[0]);
+   }
+   if(arr[n-1]>arr[n-2]){
+            printf("%d ",arr[n-1]);
+        }
+}
