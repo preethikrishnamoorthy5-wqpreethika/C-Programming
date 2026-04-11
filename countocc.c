23ba996b1eaba8b66faafc564b11597ae4749e77
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-12 00:05:13 +0530

    count the occurance of the number.

--- countocc.c
+++ countocc.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main(){
-    int n;
-    scanf("%d",&n);
+    int n,occ;
+    scanf("%d %d",&n,&occ);
     int arr[n];
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
     int count=0;
     for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]==arr[j]){
+        
+            if(arr[i]==occ){
                 count++;
             }
-        }
+        
     }printf("%d",count);
     printf("\n");
     int sum=0;
