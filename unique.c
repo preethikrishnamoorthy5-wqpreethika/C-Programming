79acfa042f5ec0a86e48e01da2695cca2c7aa771
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-12 11:05:58 +0530

    Print the unique values.

---
+++ unique.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int found=0;
+    for(int i=0;i<n;i++){
+        int count=0;
+        for(int j=0;j<n;j++){
+            if(arr[i]==arr[j]){
+                count++;
+            }
+        }
+        if(count==1){
+        printf("%d ",arr[i]);
+    found=1;}
+    }
+    if(found==0){
+        printf("Not found");
+    }
+   
+}
