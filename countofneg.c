3f09249705a537edf0eb6930db41a6860158cfac
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-18 09:19:53 +0530

    Count of negative numbers .

---
+++ countofneg.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+        int count=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]<0){
+            count++;
+        }
+    }
+    if(count!=0){
+    printf("%d",count);
+    }
+    if(count==0){
+        printf("No negative numbers.");
+    }
+    
+}
