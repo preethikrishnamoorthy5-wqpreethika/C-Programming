ef372bd7e8b75fbcfead9abb0567e84b27783982
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-18 09:53:31 +0530

    Print max and second max element.

---
+++ twomax.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    } 
+    int max=arr[0],max1=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max1=max;
+            max=arr[i];
+        }
+        if(arr[i]>max1 && arr[i]<max){
+            max1=arr[i];
+        }
+    }
+    printf("%d %d ",max,max1);
+}
