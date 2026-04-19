d5431e85d27d55b0281ce3e358529ecbca5a4c80
preethika <preethikrishnamoorthy5@gmail.com>
2026-04-19 16:44:13 +0530

    Finding avg.

---
+++ avg.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+        int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    int avg=sum/n;
+    printf("%.2f",(float)avg);
+    
+}
